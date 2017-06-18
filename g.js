af2b15109febd533f9e2d4fb452325878bd7b8bd
vera <vera@kassel-Home.kassel>
2017-06-18 21:06:27 +0200

    stable begin

--- g.js
+++ g.js
@@ -3,6 +3,7 @@ var fs = require('fs'),
 	colors = require('colors'),
 	xpath = require('xpath'),
 	iconv  = require('iconv-lite'),
+	jsonfile =require('jsonfile'),
 	HtmlDom = require('htmldom'),
 	cheerio = require('cheerio'),
 	dom = require('xmldom').DOMParser,
@@ -18,6 +19,31 @@ var fs = require('fs'),
 	getArtistTitle = require("get-artist-title"),
 	request = require('request');
 
+var f = process.argv.pop();
+
+var construct = function(){
+
+	if (fs.existsSync(process.cwd()+'/'+f+'.json')) {
+		jsonfile.readFile(process.cwd()+'/'+f+'.json', function(err, obj) {
+		  if(err) return false;
+		  initializeNormal(obj.creator, obj.alt_title);
+		});
+	}else{
+		let [artist, title] = getArtistTitle(f);
+		if(!artist && !title){
+			fillData();
+		}else{
+			initializeNormal(artist, title);
+		}
+	}
+	
+}
+var normalizeGoogle = function(str){
+	return str.replace(new RegExp(/\,/, 'g'), '').replace(new RegExp(/\'/, 'g'), '').replace(new RegExp(/\?/, 'g'), '').split(' ').join('+');
+}
+var normalizeGenius = function(str){
+	return str.replace(new RegExp(/\,/, 'g'), '').replace(new RegExp(/\./, 'g'), '').replace(new RegExp(/\'/, 'g'), '').replace(/\?/, '').split(' ').join('-');
+}
 var move = function(oldPath, newPath){
 	fs.rename(oldPath, newPath, function (err) {
 	  if (err) console.log( err.red, oldPath, newPath);
@@ -30,84 +56,85 @@ var mkdirSync = function (dirPath, callback) {
 	    else callback();
 	});
 }
-function getData(body, error, callback){
-	var _str = 'https://genius.com/'+artist.split(' ').join('-')+'-'+title.split(' ').join('-')+'-lyrics';
+function getData(artist, title, body, error, callback){
+	var _str = 'https://genius.com/'+normalizeGenius(artist, '-')+'-'+normalizeGenius(title, '-')+'-lyrics';
 	request({
 		url: _str, 
 		encoding: null
 	},
 	function (_error, _response, _body) {
+		console.log(_str);
 		if(_error){
 			fillData();
 			return false;
 		}
 		var $  = cheerio.load(iconv.decode(new Buffer(body), "ISO-8859-1"));
-  		var album = $('span:contains("Álbum")').parent().find('span').eq(1).find('a').text();
+		var $$ = cheerio.load(iconv.decode(new Buffer(_body), "ISO-8859-1"));
+		var album = $('span:contains("Álbum")').parent().find('span').eq(1).find('a').text();
   		var fecha = $('span:contains("Fecha de lanzamiento")').parent().find('span').eq(1).text();
   		var genre = $('span:contains("Género")').parent().find('span').eq(1).text();
-  		var $$ = cheerio.load(iconv.decode(new Buffer(_body), "ISO-8859-1"));
-  		var cover_Art =  $$('div.cover_art').find('img').eq(0).attr('src');
-		callback({
-			artist: artist,
-			title: title,
-  			album: album,
-  			year: fecha,
-  			genre: genre
+		if($$('h1').eq(0).text()=='Oops! Page not found'){
+			fillData(album, fecha, genre);
+		}else{
+	  		var cover_Art =  $$('div.cover_art').find('img').eq(0).attr('src');
+			callback({
+				artist: artist,
+				title: title,
+	  			album: album,
+	  			year: fecha,
+	  			genre: genre
 
-  		}, cover_Art);
+	  		}, cover_Art);
+		}
 
 	});
 }
+var appendId3Tags = function(id3, cover){
+	wiriteId(id3, cover, function(meta, file, name){
+		var dirPath = process.cwd()+'/'+meta.artist.replace(/\b\w/g, function(l){ return l.toUpperCase() })+'/'+meta.album.replace(/\b\w/g, function(l){ return l.toUpperCase() });
+		mkdirSync(dirPath, function() {
+			move(
+				file, 
+				dirPath+'/'+name+'.mp3'
+			);
+			fs.unlink(process.cwd()+'/'+name+'.json');
+		});
+	});
+};
 
-var f = process.argv.pop();
-let [artist, title] = getArtistTitle(f);
 
-if(!artist && !title){
-	fillData();
-}else{
-	initializeNormal();
-}
 
-function initializeNormal(){
+
+
+function initializeNormal(artist, title){
 	var str = 'https://www.google.es/search?q=';
-	str+=artist.split(' ').join('+')+'+'+title.split(' ').join('+');
+	str+=normalizeGoogle(artist)+'+'+normalizeGoogle(title);
 	request({
 		url: str, 
 		encoding: null
 		},
 		function (error, response, body) {
-	  		getData(body, error, function(id3, uriCover){
-	  			getCover(uriCover, id3, function(id3){
-	  				wiriteId(id3, function(meta, _iF){
-	  					var dirPath =process.cwd()+'/'+meta.artist.replace(/\b\w/g, function(l){ return l.toUpperCase() })+'/'+meta.album.replace(/\b\w/g, function(l){ return l.toUpperCase() });
-	  					var _file = _iF;
-	  					mkdirSync(dirPath, function(){
-	  						move(
-	  							process.cwd()+'/'+_file+'.mp3', 
-	  							dirPath+'/'+_file+'.mp3'
-	  						);
-	  					});
-	  				});
-	  			});
+	  		getData(artist, title, body, error, function(id3, uriCover){
+	  			getCover(uriCover, function(coverFile){
+					appendId3Tags(id3, coverFile)
+				});
 	  		});
 	  	}
 	);
 }
 
 
-function getCover(uri, tag, callback){
+function getCover(uri, callback){
 	try{
 		request(uri).pipe(fs.createWriteStream(process.cwd()+'/tmp.jpg').on('close', function(){
-			callback(tag)
+			callback(process.cwd()+'/tmp.jpg')
 		}));
 	}catch (err) {
-	  // Handle the error here.
 	  fillData();
 	}
 	
 }
 function fillData(){
-	console.log(f.green);
 	var questions = [
 	  {
 	    type: 'input',
@@ -137,27 +164,16 @@ function fillData(){
 	inquirer.prompt(questions)
 	.then(function (answers) {
 		thenSearch(answers.author, answers.title, function(e){
-			console.log('fin it', e);
-			getCover(e.cover, e, function(id3){
-  				wiriteId(id3, function(meta, _iF){
-  					var dirPath =process.cwd()+'/'+meta.artist.replace(/\b\w/g, function(l){ return l.toUpperCase() })+'/'+meta.album.replace(/\b\w/g, function(l){ return l.toUpperCase() });
-  					var _file = _iF;
-  					mkdirSync(dirPath, function(){
-  						move(
-  							process.cwd()+'/'+_file+'.mp3', 
-  							dirPath+'/'+_file+'.mp3'
-  						);
-  					});
-  				});
-  			});
+			getCover(e.cover, function(coverFile){
+				appendId3Tags(e, coverFile)
+			});
 		});
 	});
 	//
 }
-function wiriteId(tag, callback){
-	var _iF  = (process.argv.length===3) ? process.argv.pop(): f;
-	var _file = process.cwd()+'/'+_iF+'.mp3';
-	console.log(_file.blue);
+function wiriteId(tag, cover, callback){
+	var _name  = (process.argv.length===3) ? process.argv.pop(): f;
+	var _file = process.cwd()+'/'+_name+'.mp3';
 	var file = new id3.File(_file);
   	var meta = new id3.Meta({
 	    artist: tag.artist,
@@ -165,15 +181,14 @@ function wiriteId(tag, callback){
 	    album: tag.album,
 	    genre: tag.genre || '',
 	    year: (tag.year)  ? Number(tag.year) : 0
-	},[new id3.Image(process.cwd()+'/tmp.jpg')]);
+	},[new id3.Image(cover)]);
 	writer.setFile(file).write(meta, function(err) {
 		if (err) {
     		console.log(err.red);
 		}else{
-			tube.pipe(process.stdout);
-			gm(process.cwd()+'/tmp.jpg').resize(240, 240);
 			console.log(`Archivo ${f}.blue creado con tags e imagen`.green);
-			callback(tag, _iF);
+			callback(tag, _file, _name);
 		}
 	});
 }
+construct();
